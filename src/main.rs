b82fc37fb4046508c968b81e8d18edc29d3b7100
user.baruu <fulan.gulan@gmail.com>
2021-03-02 11:45:05 +0700

    modifikasi terapan fungsi firmware

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use tokio::net::TcpStream;
-use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use crate::app::error::AppError;
+use crate::app::AppError;
+use crate::app::helpers::firmware;
 
 mod app;
 
@@ -8,14 +7,5 @@ mod app;
 async fn main() -> Result<(), AppError> {
     println!("Connecting to Initium");
 
-    let mut stream = TcpStream::connect("192.168.129.119:8400").await?;
-    let command = b"LA4 111;";
-    stream.write_all(command).await?;
-
-    let mut buffer = [0; 8];
-    stream.read(&mut buffer).await?;
-
-    println!("The firmware of the DTC Initium: {:?}", buffer);
-
-    Ok(())
+    firmware::get_version().await
 }
