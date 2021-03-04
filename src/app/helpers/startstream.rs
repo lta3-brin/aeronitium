30105dd264832e95922d3b201f99d5eebcb6970d
user.baruu <fulan.gulan@gmail.com>
2021-03-04 16:41:41 +0700

    Membuat fungsi awal start/stop stream

---
+++ src/app/helpers/startstream.rs
@@ -0,0 +1,20 @@
+use tokio::net::TcpStream;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use crate::app::AppError;
+use crate::app::models::SimpleMessage;
+use crate::app::helpers::display;
+
+
+#[allow(dead_code)]
+pub async fn command(setup_table: u8) -> Result<SimpleMessage<i32>, AppError> {
+    let mut stream = TcpStream::connect("192.168.129.119:8400").await?;
+    let cmd = format!("AD2 {};", setup_table);
+    let mut buffer = [0; 8];
+
+    stream.write_all(cmd.as_bytes()).await?;
+    stream.read(&mut buffer).await?;
+
+    let message = display::display_message::<i32>(buffer)?;
+
+    Ok(message)
+}
