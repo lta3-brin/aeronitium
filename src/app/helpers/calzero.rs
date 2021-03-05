738aaf7af8560897fae8da1f9a430ab20be66789
user.baruu <fulan.gulan@gmail.com>
2021-03-05 16:57:14 +0700

    Penyesuaian fungsi kalibrasi Re-Zero

--- src/app/helpers/calzero.rs
+++ src/app/helpers/calzero.rs
@@ -6,10 +6,11 @@ use crate::app::helpers::display;
 
 
 #[allow(dead_code)]
-pub async fn command() -> Result<SimpleMessage<String>, AppError> {
-    let mut stream = TcpStream::connect("192.168.129.119:8400").await?;
+pub async fn command(
+    stream: &mut TcpStream,
+    mut buffer: [u8; 8]
+) -> Result<SimpleMessage<String>, AppError> {
     let command = b"CA2;";
-    let mut buffer = [0; 8];
 
     stream.write_all(command).await?;
     stream.read(&mut buffer).await?;
