6c26c9177eebd6a64137846a27e518e2dac67298
user.baruu <fulan.gulan@gmail.com>
2021-03-10 13:50:39 +0700

    Modifikasi parameter akuisisi data tekanan

--- src/app/helpers/simplesetup.rs
+++ src/app/helpers/simplesetup.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc};
 use tokio::{net::TcpStream, sync::Mutex};
 use crate::app::AppError;
 use crate::app::helpers::{
@@ -7,7 +8,6 @@ use crate::app::helpers::{
     stream,
     // calzero
 };
-use std::sync::{Arc};
 
 
 #[allow(dead_code)]
@@ -20,22 +20,21 @@ pub async fn command() -> Result<(), AppError> {
     const NUM_CHANNELS: u8 = 64;  //number of channels/port on the ESP. Allowed values include 32, 48, or 64
     const SCN_ADDRESS: u8  = 1;  // scanner address (1-8)
     const LRN: u8          = 1;  // logical range number
-    const STBL_1: u8       = 1;  // filtered and averaged scanner table used (1-4, 5 is Initium generated)
-    const STBL_2: u8       = 2;  // single point scanner table
+    const STBL: u8         = 2;  // single point scanner table
     const SPORT: &str      = "101-164";  // 64 ports on first scanner
 
     // measurement settings
-    const NFR_1: u8  = 64;  // number of frames averaged over for each measurement set
-    const NFR_2: u8  = 1;  // no averaging -> rapid scanning for signal processing
-    const FRD: u8    = 0;  // frame delay (should be 0)
-    const NMS_1: u16  = 1;  // number of pressure measurement sets to take (0 = continuous until aborted)
-    const NMS_2: u16  = 100;  // return a large set of frames to user
-    const MSD_1: u16 = 500;  // milliseconds between start of sequential pressure measurement sets
-    const MSD_2: u16  = 0;  // milliseconds between start of sequential pressure measurement sets
-    const TRM: &str  = "FREE";  // trigger mode - free ignores trigger
-    const SCM: &str  = "PAM";  // scan mode - parrallel address mode scans ports concurrently
-    const OCF: u8    = 2;  // output convert format (2 = normal EU pressure)
-    const UNX: u8    = 3;  // units index (1 = psi, 3 = Pa, 6 = atm, 7 = mmHg, 8 = mmH20, 9 = bar, 10 = kPa, 11 = mbar)
+    const NFR: u8          = 1;  // no averaging -> rapid scanning for signal processing
+    const FRD: u8          = 0;  // frame delay (should be 0)
+    const NMS: u16         = 0;  // continuous pressure measurement sets to take until aborted
+    const MSD: u16       = 5000;  // milliseconds between start of sequential pressure measurement sets
+    const TRM: &str        = "FREE";  // trigger mode - free ignores trigger
+    const SCM: &str        = "PAM";  // scan mode - parrallel address mode scans ports concurrently
+    const OCF: u8          = 2;  // output convert format (2 = normal EU pressure)
+    const UNX: u8          = 3;  // units index (1 = psi, 3 = Pa, 6 = atm, 7 = mmHg, 8 = mmH20, 9 = bar, 10 = kPa, 11 = mbar)
+
+    // let x = calzero::command(&mut stream, buffer).await?;
+    // println!("{:?}", x);
 
     // define connected scanners
     let m = initialization::check(
@@ -48,51 +47,28 @@ pub async fn command() -> Result<(), AppError> {
     ).await?;
     println!("{:?}", m);
 
-    // define data acquisition parameters for filtered table
-    // let m = daparams::check(
-    //     &mut stream,
-    //     buffer,
-    //     CRS,
-    //     STBL_1,
-    //     NFR_1,
-    //     FRD,
-    //     NMS_1,
-    //     MSD_1,
-    //     TRM,
-    //     SCM,
-    //     OCF
-    // ).await?;
-
     // define data acquisition parameters for rapid scanning table
     let m = initialization::define(
         &mut stream,
         buffer,
         CRS,
-        STBL_2,
-        NFR_2,
+        STBL,
+        NFR,
         FRD,
-        NMS_2,
-        MSD_2,
+        NMS,
+        MSD,
         TRM,
         SCM,
         OCF
     ).await?;
     println!("{:?}", m);
 
-    // define scan list for filtered table
-    // let cmd = format!(
-    //     "SD3 {} {} {};", CRS, STBL_1, SPORT
-    // );
-    // stream.write_all(cmd.as_bytes()).await?;
-    // stream.read(&mut buffer).await?;
-    // let message = display::display_message::<i32>(buffer)?;
-
     // define scan list for rapid scanning
     let m = initialization::scan(
         &mut stream,
         buffer,
         CRS,
-        STBL_2,
+        STBL,
         SPORT
     ).await?;
     println!("{:?}", m);
@@ -138,14 +114,12 @@ pub async fn command() -> Result<(), AppError> {
         &mut stream,
         buffer,
         CRS,
-        STBL_2,
+        STBL,
         SPORT
     ).await?;
 
-    // let x = calzero::command(&mut stream, buffer).await?;
-    // println!("{:?}", x);
     let stream = Arc::new(Mutex::new(stream));
-    stream::daq(stream, STBL_2).await?;
+    stream::daq(stream, STBL_2).await;
 
     Ok(())
 }
