ca3ab3add4cd3b76b7b66fcacfbecdf6f6bc8bfa
user.baruu <fulan.gulan@gmail.com>
2021-03-16 16:03:27 +0700

    Hapus module handler dan routers yang tidak digunakan

--- src/app/handlers/root.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::{get, HttpResponse};
-
-
-#[get("/")]
-pub fn root_handler() -> HttpResponse {
-    HttpResponse::Ok().body("Halaman ini sengaja dikosongkan")
-}
--- src/app/handlers/version.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::{get, HttpResponse};
-
-
-#[get("/")]
-pub fn version_one() -> HttpResponse {
-    HttpResponse::Ok().body("Ini untuk router v1")
-}
