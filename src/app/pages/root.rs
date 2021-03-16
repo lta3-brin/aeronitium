1b0bab575539eb3d9a24977f40eb05e5dc33a6ab
user.baruu <fulan.gulan@gmail.com>
2021-03-17 06:36:16 +0700

    Menambahkan keterangan penggunaan aplikasi

--- src/app/pages/root.rs
+++ src/app/pages/root.rs
@@ -6,12 +6,9 @@ use cursive::align::Align;
 pub fn root_page() -> NamedView<StackView> {
     let widget = StackView::new()
         .layer(
-            Dialog::text("Layer 2").full_screen()
-        )
-        .layer(
             Dialog::around(TextView::new(
                 "Pilih prosedur yang telah disediakan disamping. \
-                Lakukanlah secara berurutan. Tekan Enter untuk memilih."
+                Lakukanlah secara berurutan. Tekan Enter atau klik kanan mouse untuk memilih."
             ).align(Align::center())).full_screen()
         )
         .with_name("pilih_page");
