9ce39ac5cd9704bf2ba4bd867cb5cfb64a2d6512
user.baruu <fulan.gulan@gmail.com>
2021-03-21 11:49:42 +0700

    Membuat fungsi handler untuk router Aeronitium

---
+++ src/app/routers.rs
@@ -0,0 +1,23 @@
+use actix_web::web;
+use crate::app::handlers::root::get_root;
+use crate::app::handlers::firmware::get_firmware;
+use crate::app::handlers::connscanner::connected_scanners;
+use crate::app::handlers::daqparams::daq_params;
+use crate::app::handlers::scanlist::scan_list;
+use crate::app::handlers::eeprom::eeprom_coef;
+use crate::app::handlers::changeunit::set_unit;
+use crate::app::handlers::scannerstatus::get_status;
+use crate::app::handlers::tablecoef::save_coef;
+
+
+pub fn app_routers(router: &mut web::ServiceConfig) {
+    router.service(get_root)
+        .service(get_firmware)
+        .service(connected_scanners)
+        .service(daq_params)
+        .service(scan_list)
+        .service(eeprom_coef)
+        .service(set_unit)
+        .service(get_status)
+        .service(save_coef);
+}
