5e91449889a325e63f0aa446c7a2acc910f5b9d0
user.baruu <fulan.gulan@gmail.com>
2021-03-21 11:49:06 +0700

    Membuat penyesuaian error handling untuk AppError

---
+++ src/app/errors/responses.rs
@@ -0,0 +1,28 @@
+use actix_web::{error, HttpResponse};
+use actix_web::http::{StatusCode, header};
+use actix_web::dev::HttpResponseBuilder;
+use crate::app::AppError;
+use crate::app::models::SimpleMessage;
+
+
+impl error::ResponseError for AppError {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+           _ => StatusCode::BAD_REQUEST,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        let message = SimpleMessage::new(
+            0,
+            String::from("Kode kesalahan dari Aeronitium"),
+            0,
+            String::from("Tipe kesalahan dari Aeronitium"),
+            self.to_string()
+        );
+
+        HttpResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "application/jeson")
+            .json(message)
+    }
+}
