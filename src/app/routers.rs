1c0eb57e993f56e7a0072f330655e1c054357dfd
user.baruu <fulan.gulan@gmail.com>
2021-03-21 12:55:12 +0700

    Menambahkan router baru rezero kedalam routers Aeronitium

--- src/app/routers.rs
+++ src/app/routers.rs
@@ -8,6 +8,7 @@ use crate::app::handlers::eeprom::eeprom_coef;
 use crate::app::handlers::changeunit::set_unit;
 use crate::app::handlers::scannerstatus::get_status;
 use crate::app::handlers::tablecoef::save_coef;
+use crate::app::handlers::rezero::calib_rezero;
 
 
 pub fn app_routers(router: &mut web::ServiceConfig) {
@@ -19,5 +20,6 @@ pub fn app_routers(router: &mut web::ServiceConfig) {
         .service(eeprom_coef)
         .service(set_unit)
         .service(get_status)
-        .service(save_coef);
+        .service(save_coef)
+        .service(calib_rezero);
 }
