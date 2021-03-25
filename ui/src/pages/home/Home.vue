24c916f906a8d2b731da156ae20ef4a52f819bf0
user.baruu <fulan.gulan@gmail.com>
2021-03-25 10:56:32 +0700

    Modifikasi layout halaman utama

--- ui/src/pages/home/Home.vue
+++ ui/src/pages/home/Home.vue
@@ -1,28 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card class="card_fixed_width">
-      <q-card-section class="q-pb-none">
-        <BannerComponent />
-        <div class="text-right full-width">
+  <q-page class="row items-center justify-center q-pa-md">
+    <div class="col-6">
+      <q-card class="text-center">
+        <q-card-section class="q-pb-none">
+          <BannerComponent class="overflow-auto" />
           <i>DTC firmware v2.02</i>
-        </div>
-      </q-card-section>
+        </q-card-section>
 
-      <q-separator inset />
+        <q-separator inset />
 
-      <q-card-section class="card_font_size">
-        <p>
-          Aplikasi monitoring sensor tekanan udara berkecepatan tinggi melalui perangkat DTC Initium untuk
-          mendukung pengujian teknologi Aero..🚀
-        </p>
-        <p>
-          <b>Aeronitium</b> dikembangkan dengan <span class="text-red">❤️</span> oleh
-          <i><a href="https://github.com/bbta3-bppt/aeronitium" rel="noreferrer nofollow" target="_blank">
-            AEROTRONIKA
-          </a></i>.
-        </p>
-      </q-card-section>
-    </q-card>
+        <br />
+        <q-card-section class="card_font_size">
+          <p>
+            Aplikasi monitoring sensor tekanan udara berkecepatan tinggi melalui perangkat DTC Initium untuk
+            mendukung pengujian teknologi Aero..🚀
+          </p>
+          <p>
+            <b>Aeronitium</b> dikembangkan dengan <span class="text-red">❤️</span> oleh
+            <a href="https://github.com/bbta3-bppt/aeronitium" rel="noreferrer nofollow" target="_blank">
+              <i>AEROTRONIKA</i>
+            </a>.
+          </p>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
