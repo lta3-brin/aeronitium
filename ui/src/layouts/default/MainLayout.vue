362b70a4202a1f5fe3877ba1560d5451e4a7a654
user.baruu <fulan.gulan@gmail.com>
2021-03-25 10:56:49 +0700

    Modifikasi layout default aplikasi

--- ui/src/layouts/default/MainLayout.vue
+++ ui/src/layouts/default/MainLayout.vue
@@ -6,32 +6,39 @@
       @click.capture="drawerClick"
 
       :breakpoint="500"
-      content-class="bg-black text-white"
+      content-class="bg-grey-10 text-white"
     >
       <q-scroll-area class="fit">
         <q-list padding>
           <q-item clickable v-ripple :active="$route.name === 'home'">
             <q-item-section avatar>
-              <q-icon name="home" />
+              <q-icon name="fas fa-home" />
             </q-item-section>
+            <q-tooltip
+              anchor="center right"
+              self="center middle"
+              content-class="bg-orange"
+              content-style="font-size: 16px;"
+              :offset="[37, 0]"
+            >
+              Utama
+            </q-tooltip>
           </q-item>
 
           <q-item clickable v-ripple>
             <q-item-section avatar>
-              <q-icon name="star" />
+              <q-icon name="fas fa-cogs" />
             </q-item-section>
-          </q-item>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="send" />
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-icon name="drafts" />
-            </q-item-section>
+            <q-tooltip
+              anchor="center right"
+              self="center left"
+              content-class="bg-orange"
+              content-style="font-size: 16px;"
+              :offset="[5, 0]"
+            >
+              Pengaturan
+            </q-tooltip>
           </q-item>
         </q-list>
       </q-scroll-area>
