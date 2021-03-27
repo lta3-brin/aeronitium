7b658971576828b749d360666a5e99a2e1146197
user.baruu <fulan.gulan@gmail.com>
2021-03-27 16:12:45 +0700

    Membuat fungsi interaksi untuk komunikasi ke Aeronitium server

--- ui/src/pages/configs/configs.js
+++ ui/src/pages/configs/configs.js
@@ -1,4 +1,8 @@
 import BannerComponent from "src/components/banner/banner.vue"
+import { check_unx } from "src/pages/configs/helpers/unx"
+import { check_ocf } from "src/pages/configs/helpers/ocf"
+import { precheck_stream } from "src/pages/configs/helpers/check_stream"
+import { do_rezero, check_scanner } from "src/pages/configs/helpers/dtc"
 
 export default {
   name: "ConfigurationsPage",
@@ -12,6 +16,8 @@ export default {
       num_channels: 64,
       num_channels_options: [32, 48, 64],
       scn_address: "1",
+      scn_number: 1,
+      scn_number_options: [1, 2, 3, 4, 5, 6, 7, 8],
       lrn: 1,
       lrn_options: [1, 2, 3, 4, 5],
       stbl: 2,
@@ -27,36 +33,39 @@ export default {
       ocf: "EU",
       ocf_options: ["raw", "EU"],
       unx: "Pa",
-      unx_options: ["psi", "Pa", "atm", "mmHg", "mmH20", "bar", "kPa", "mbar"]
+      unx_options: ["psi", "Pa", "atm", "mmHg", "mmH20", "bar", "kPa", "mbar"],
+      isMounted: false,
+      dtc_dialog: false,
+      show_button_dialog: false,
+      payload: {}
     }
   },
   methods: {
-    check_ocf(ocf) {
-      switch (ocf) {
-        case "raw":
-          return 1
-        case "EU":
-          return 2
-      }
+    check_unx,
+    check_ocf,
+    close_dialog() {
+      this.dtc_dialog = false
+      this.show_button_dialog = false
     },
-    check_unx(unx) {
-      switch (unx) {
-        case "psi":
-          return 1
-        case "Pa":
-          return 3
-        case "atm":
-          return 6
-        case "mmHg":
-          return 7
-        case "mmH20":
-          return 8
-        case "bar":
-          return 9
-        case "kPa":
-          return 10
-        case "mbar":
-          return 11
+    async dtc_command(number) {
+      this.dtc_dialog = true
+
+      switch (number) {
+        case 1:
+          this.payload = await do_rezero(this.lrn)
+          break
+        case 2:
+          this.payload = await check_scanner(this.crs, this.scn_number)
+      }
+
+      this.show_button_dialog = true
+    }
+  },
+  mounted() { this.isMounted = true },
+  computed: {
+    precheck_stream() {
+      if (this.isMounted) {
+        return precheck_stream(this.$refs)
       }
     }
   }
