6481b1a6867c2f315fd73c5fe3b786e632343775
user.baruu <fulan.gulan@gmail.com>
2021-03-27 18:29:27 +0700

    Membuat fungsi komunikasi ke Aeronitium server untuk konfigurasi stream

--- ui/src/pages/configs/helpers/dtc.js
+++ ui/src/pages/configs/helpers/dtc.js
@@ -31,3 +31,34 @@ export const check_scanner = async (crs, scanner_number) => {
     }
   }
 }
+
+export const setup_stream = async (payload) => {
+  const URL = `${process.env.APP_ADDRESS}/simplesetup`
+
+  try {
+    const res = await axios.post(URL, {
+      crs: payload.crs,
+      num_channels: payload.num_channels,
+      scn_address: payload.scn_address,
+      lrn: payload.lrn,
+      stbl: payload.stbl,
+      sport: payload.sport,
+      nfr: payload.nfr,
+      frd: payload.frd,
+      nms: payload.nms,
+      msd: payload.msd,
+      trm: payload.trm,
+      scm: payload.scm,
+      ocf: payload.ocf,
+      unx: payload.unx
+    })
+
+    return res.data
+  } catch (err) {
+    return {
+      code_message: "DTC commands",
+      kind_message: "Error packet: indicates that command was executed with error",
+      data: err.message,
+    }
+  }
+}
