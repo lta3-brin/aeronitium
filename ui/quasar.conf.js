415e33baaed99fcf021700c30b04f4c30546bc3d
user.baruu <fulan.gulan@gmail.com>
2021-03-27 16:16:47 +0700

    Memberikan nilai env kedalam AeronitiumBBTA3

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -69,13 +69,18 @@ module.exports = function (/* ctx */) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: [ 'js', 'vue' ] }])
       },
+
+      // https://quasar.dev/quasar-cli/handling-process-env#adding-to-process-env
+      env: {
+        APP_ADDRESS: process.env.APP_ADDRESS || 'http://localhost:8080'
+      }
     },
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
-      open: true // opens browser window automatically
+      port: 8181,
+      open: false // opens browser window automatically
     },
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
