93ebc3a4ed36679084bcfff4b116a5273007913f
user.baruu <fulan.gulan@gmail.com>
2021-03-30 15:57:35 +0700

    Membuat state koneksi nats kedalam Aeronitium

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use actix_web::rt::net::TcpStream;
 use actix_web::{App, HttpServer, web};
 use crate::app::AppError;
 use crate::app::configs::get_configs;
-use crate::app::helpers::display::display_banner;
 use crate::app::routers::app_routers;
+use crate::app::helpers::process::run_script;
+use crate::app::helpers::display::display_banner;
 
 
 #[actix_web::main]
@@ -18,14 +19,21 @@ async fn main() -> Result<(), AppError> {
         TcpStream::connect(conf.get_dtc_addr()).await?
     ));
 
+    let nats_conn = web::Data::new(Mutex::new(
+        async_nats::connect(&*conf.get_nats_addr()).await?
+    ));
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(Cors::default().allow_any_origin().allow_any_header().allow_any_method())
+            .app_data(nats_conn.clone())
             .app_data(tcp_conn.clone())
             .configure(app_routers)
     }).bind(conf.get_server_addr())?;
 
     println!("{}", display_banner());
-    println!("Starting on {}", conf.get_server_addr().clone());
+    println!("Menjalankan server di {}", conf.get_server_addr().clone());
+    run_script()?;
+
     Ok(server.run().await?)
 }
