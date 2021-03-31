453d6c0499c389e36395b14912306337f11d5947
user.baruu <fulan.gulan@gmail.com>
2021-03-31 16:02:36 +0700

    Daftar route baru stream kedalam router

--- ui/src/router/routes.js
+++ ui/src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/default/MainLayout.vue'),
     children: [
       { path: '', name: 'home', component: () => import('pages/home/Home.vue') },
-      { path: '/configs', name: 'configs', component: () => import('pages/configs/Configurations.vue') }
+      { path: '/configs', name: 'configs', component: () => import('pages/configs/Configurations.vue') },
+      { path: '/stream', name: 'stream', component: () => import('pages/stream/Stream.vue') }
     ]
   },
 
