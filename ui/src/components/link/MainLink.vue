56cff33e0d4fbc490bf3eebcc7686229415b07d3
user.baruu <fulan.gulan@gmail.com>
2021-03-31 16:01:27 +0700

    Atur atribut komponen navigasi

---
+++ ui/src/components/link/MainLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-item clickable v-ripple :to="path"
+          :active="$route.name === nameUrl"
+          :exact="$route.name !== nameUrl">
+    <q-item-section avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+    <q-tooltip
+      anchor="center right"
+      self="center middle"
+      content-class="bg-orange"
+      content-style="font-size: 16px;"
+      :offset="[37, 0]"
+    >
+      {{ title }}
+    </q-tooltip>
+  </q-item>
+</template>
+
+<script src="./link.js" />
