9bb015dd171967c6f28fb42ec0ba0d9c95d7ebe2
user.baruu <fulan.gulan@gmail.com>
2021-03-31 16:02:49 +0700

    Membuat halaman stream aplikasi

---
+++ ui/src/pages/stream/Stream.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="row items-center justify-center">
+    <div class="col-12 q-px-md">
+      <q-card dark class="text-center bg-grey-9">
+        <q-card-section class="q-pb-none">
+          <BannerComponent />
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section>
+          <Plotly :data="data" :layout="layout" :displaylogo="false" />
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script src="./streaming.js" />
