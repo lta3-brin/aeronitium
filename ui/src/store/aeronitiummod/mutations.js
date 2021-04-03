def5f05a40ec8efa5e1684a7cc5d8879b29ec6d6
user.baruu <fulan.gulan@gmail.com>
2021-04-03 16:56:12 +0700

    Membuat fungsi state mutation status stream

--- ui/src/store/aeronitiummod/mutations.js
+++ ui/src/store/aeronitiummod/mutations.js
@@ -1,3 +1,7 @@
 export function dtcMutation (state, payload) {
   state.dtc.stbl = payload.stbl
 }
+
+export function stremReadyMutation (state, payload) {
+  state.dtc.ready_stream = payload.ready_stream
+}
