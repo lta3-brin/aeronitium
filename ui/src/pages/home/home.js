fc7cedc617c9513f8d4fa1d9830dbfcc859b8690
user.baruu <fulan.gulan@gmail.com>
2021-04-04 12:37:47 +0700

    Menerapkan variabel dinamis untuk server dan websocket

--- ui/src/pages/home/home.js
+++ ui/src/pages/home/home.js
@@ -6,6 +6,28 @@ export default {
   components: {
     BannerComponent
   },
+  computed: {
+    server: {
+      get: function() {
+        const data = this.$store.getters["aeronitiummod/addressGetter"]
+
+        return data.server
+      },
+      set: function (val) {
+        this.$store.commit("aeronitiummod/addressServerMutation", val)
+      }
+    },
+    websocket: {
+      get: function() {
+        const data = this.$store.getters["aeronitiummod/addressGetter"]
+
+        return data.websocket
+      },
+      set: function (val) {
+        this.$store.commit("aeronitiummod/addressWebsocketMutation", val)
+      }
+    }
+  },
   methods: {
     onAbout(e) {
       openURL("https://github.com/bbta3-bppt/aeronitium", null, {
